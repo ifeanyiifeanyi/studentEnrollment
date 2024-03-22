429c633fd868fd7309860ecb2b6301d809491990
Nnaemego Ifeanyi <josephifeanyid2@gmail.com>
2024-03-22 11:31:52 +0100

    changed all the auto files view3

--- resources/views/student/layouts/studentLayout.blade.php
+++ resources/views/student/layouts/studentLayout.blade.php
@@ -47,7 +47,7 @@
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="{{ route(" student.dashboard") }}">Home</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route("student.dashboard") }}">Home</a></li>
                                 <li class="breadcrumb-item active">@yield("title")</li>
                             </ol>
                         </div><!-- /.col -->
