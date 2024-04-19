d5784822820b05a45a5010906c7c9b6f53c594dc
Nnaemego Ifeanyi <josephifeanyid2@gmail.com>
2024-04-20 00:19:42 +0100

    manage student profile admin

--- main.java
+++
@@ -1,9 +0,0 @@
-class Square {
-    public static void printSquare(String x){
-        System.out.println(x);
-    }
-
-    public static void main(String[] arguments){
-        printSquare("Hello world");
-    }
-}
